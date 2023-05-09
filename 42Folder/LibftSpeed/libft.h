e3cd6a9af2d732a294b0b35a68b03ffef606da6b
Larcrist <larcrist@student.42sp.org.nr>
2023-05-09 17:53:39 -0300

    done : manipulate characters part

---
+++ 42Folder/LibftSpeed/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larcrist <larcrist@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 17:15:35 by larcrist          #+#    #+#             */
+/*   Updated: 2023/05/09 17:52:45 by larcrist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isupper(int c);
+int	ft_islower(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+size_t	ft_strlen(char *str);
+
+#endif
